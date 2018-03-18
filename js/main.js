c995ba94265396e1f493b038f349059760143b06
dima <dmitriy.dubchenko@gmail.com>
2018-03-18 14:39:41 +0200

    commit  18/03/18

--- js/main.js
+++ js/main.js
@@ -5,64 +5,165 @@ document.addEventListener("DOMContentLoaded", function() {
 class Calculator {
     constructor(obj){
         this.DOMs = obj;
-        this.result = "";
-        this.temp = 0;
+				this.expression = "";
+				this.memory = "";
+				this.equals = "0";
+				this.numberBtns = [this.DOMs.zero, this.DOMs.one, this.DOMs.two, this.DOMs.three, this.DOMs.four,											this.DOMs.fife, this.DOMs.six, this.DOMs.seven,this.DOMs.eight,this.DOMs.nine]
     }
+
+
 
     display(event){
-        let temp = 0;
-        if(event.target === this.DOMs.display) {
-            return false;
-        }
-        if(event.target.value === "AC" ){
-            this.DOMs.display.value = "";
-            this.result = "";
-            return false;
-        }
-        if(event.target.value === "="){
-            this.temp = eval(this.result);
-            if(this.temp == "undefined"){
-                return false;
-            }
-            console.log(this.temp);
-            this.DOMs.display.value = this.temp;
-            this.result = this.temp;
-            event.preventDefault();
-            return false;
-        }
-        
+			if(event.target.type !== "button"  ) {
+				return;
+			}
 
 
-        //this.DOMs.display.value = "";
-        this.result +=  event.target.value;
+			
+			// if(event.target == this.DOMs.display) {
+			// 		return ;
+			// }
+
+			// if(event.target.value >= 0 && event.target.value <= 9 ) {
+			// 	this.DOMs.display.value = "";
+			// 	//this.expression = "";
+			// }
+
+			if(event.target === this.DOMs.backspace) {
+				this.expression = this.DOMs.display.value;
+				this.expression = this.expression.substring(0, this.expression.length - 1);
+				this.DOMs.display.value = this.expression;
+				return false;
+			}
+			
+			// if(event.target === this.DOMs.persent) {
+			// 	let temp = this.expression.replace("%" , "/")
+			// 	//this.expression = this.expression.substring(0, this.expression.length - 1);
+			// 	this.DOMs.display.value = this.expression;
+			// 	return false;
+			// }
+
+
+			if (event.target == this.DOMs.memory){
+				if(this.memory == ""){
+					this.DOMs.display.value = "";
+					return;
+				}
+				var temp = eval(this.memory);
+				this.DOMs.display.value = temp;
+				this.expression += this.memory;
+					return ;				
+			}
+			if (event.target == this.DOMs.mplus){
+			  if(this.expression == "") {
+					this.memory += "";
+					return ;
+				}
+				this.memory += eval(this.expression); 
+				this.expression = "";
+				this.DOMs.display.value = "";
+					return ;				
+			}
+			if (event.target == this.DOMs.mminus){
+			  if(this.expression == "") {
+					this.memory += 0;
+					return ;
+				}
+				this.memory -= eval(this.expression); 
+				this.expression = "";
+					return ;				
+			}
+			
+			if (event.target.value == "AC"){
+				this.memory = "";
+				this.DOMs.display.value = "";
+				this.expression = "";
+				return false;				
+			}
+			if(event.target.value === "CE" ){
+					this.DOMs.display.value = "";
+					this.expression = "";
+					return false;
+			}
+			if(event.target.value === "="){
+				if(this.expression == ""){
+					this.DOMs.display.value = "";
+					return;
+				}
+				var temp;
+				if(this.expression.includes("%")){
+					this.expression = this.expression.replace("%" , "/") + "*100";
+				}
+				temp = eval(this.expression); 
+				if(temp == "Infinity"){
+					return;
+				}
+				this.DOMs.display.value = temp; 
+				this.expression = temp.toString();
+				this.equals = "1";
+				return;
+			}
+			if (this.equals == "0"){
 				this.DOMs.display.value += event.target.value;
-				
-
-
-        
-        //console.log(this.result);
-
-
-        //event.target.value == "=" || 
-    }
+				this.expression +=  event.target.value; 
+			}
+			if (this.equals == "1"){
+				if(this.numberBtns.includes(event.target)){
+					this.DOMs.display.value = event.target.value;
+					this.expression = event.target.value;
+					this.equals = "0"
+					return;	 	
+				}	
+				this.DOMs.display.value += event.target.value;
+				this.expression += event.target.value; 
+				this.equals = "0"
+			}
+			
+		};
+		displayunicode (e){
+			var unicode = e.keyCode? e.keyCode : e.charCode
+			console.log(unicode);
+			}
+		
     
     initListeners(){
-			document.addEventListener('keypress', function(eventObject) {
-				if (!(((eventObject.which >= 48) && 
-					(eventObject.which <= 57)) 
-					|| (eventObject.which == 32)
-					|| (eventObject.which == 8)
-					|| (eventObject.which == 45) 
-					|| (eventObject.which == 40) 
-					|| (eventObject.which == 41) 
-					|| (eventObject.which == 43))) {
-					eventObject.preventDefault();
-				}
-				// var str = $("#phone").val();
-			});
-			
-			this.DOMs.calc.addEventListener("click", this.display.bind(this));
 			this.DOMs.display.focus();
+			this.DOMs.calc.addEventListener("keydown", this.displayunicode.bind(this));
+			this.DOMs.calc.addEventListener("click", this.display.bind(this));
+
+			// document.addEventListener("keydown", function(event) {
+			// 	console.log(event.which);
+			// })
+
+			
+
+			// document.addEventListener('keypress', function(eventObject) {
+			// 	if (!
+			// 				(
+			// 					((eventObject.which >= 48) && 
+			// 						(eventObject.which <= 57))
+			// 					|| (eventObject.which == 13) 
+			// 					|| (eventObject.which == 32)
+			// 					|| (eventObject.which == 8)
+			// 					|| (eventObject.which == 45) 
+			// 					|| (eventObject.which == 40) 
+			// 					|| (eventObject.which == 41) 
+			// 					|| (eventObject.which == 43)
+			// 					|| (eventObject.which == 106)
+			// 					|| (eventObject.which == 107)
+			// 					|| (eventObject.which == 187)
+			// 					|| (eventObject.which == 109)
+			// 					|| (eventObject.which == 189)
+			// 					|| (eventObject.which == 189)
+				
+			// 				)) {
+			// 		//console.log()
+			// 		eventObject.preventDefault();
+			// 	}
+			// });
+
+			
+			
     }
     
     
@@ -73,7 +174,7 @@ let calculator = new Calculator({
     leftBracket : document.querySelector("#left-bracket"),
     rightBracket : document.querySelector("#right-bracket"),
     persent : document.querySelector("#persent"),
-    AC : document.querySelector("#AC"),
+    CE : document.querySelector("#CE"),
     seven : document.querySelector("#seven"),
     eight : document.querySelector("#eight"),
     nine : document.querySelector("#nine"),
@@ -89,8 +190,12 @@ let calculator = new Calculator({
     zero : document.querySelector("#zero"),
     dot : document.querySelector("#dot"),
     equally : document.querySelector("#equally"),
-    plus : document.querySelector("#plus")
-    
+		plus : document.querySelector("#plus"),
+		memory : document.querySelector("#memory"),
+		AC : document.querySelector("#ac"),
+		mplus : document.querySelector("#mplus"),
+		mminus : document.querySelector("#mminus"),
+    backspace : document.querySelector("#backspace"),
     
 })    
 
